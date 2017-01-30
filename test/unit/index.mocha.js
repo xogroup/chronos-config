fe5a42b80b0f15889902b72c8e30b43c04c94e82
Rob Horrigan <rhorrigan@xogrp.com>
2017-01-30 16:12:07 -0600

    Update linting to ES6 and add Joi validation

--- test/unit/index.mocha.js
+++ test/unit/index.mocha.js
@@ -1,50 +1,76 @@
 'use strict';
 
-var config = require('../../lib/index')
-var chai   = require('chai');
-var should = chai.should();
+const config = require('../../lib/index');
+const { should }  = require('chai').should(); // eslint-disable-line no-unused-vars
 
 describe('unit tests - index', function() {
-    it('should load a config and assign properties', function() {
-        var context = {
-            configPath: 'test/mocks/config/sample-config.json',
+    it('should load a config and assign properties', () => {
+        const context = {
+            configPath: 'test/mocks/config/sample-config.json'
         };
 
         return config(context)
-            .then(function(config){
-                config.should.have.property('name');
-                config.name.should.equal('Sample-Config');
+            .then((config) => {
+                config.should.have.property('chronos');
+                config.chronos.should.have.property('routes');
+                config.chronos.routes[0].should.have.property('path');
+                config.chronos.routes[0].path.should.equal('/rfq');
 
-                config.should.have.property('message');
-                config.message.should.equal('This is a sample configuration');
-            })
+                config.chronos.routes[0].should.have.property('httpAction');
+                config.chronos.routes[0].httpAction.should.equal('GET');
+
+                config.chronos.routes[0].should.have.property('tags');
+                config.chronos.routes[0].tags[0].should.equal('api');
+
+                config.chronos.routes[0].should.have.property('description');
+                config.chronos.routes[0].description.should.equal('RFQ endpoint for all clients in XO');
+
+                config.chronos.routes[0].should.have.property('actions');
+                config.chronos.routes[0].actions[0].should.have.property('name');
+                config.chronos.routes[0].actions[0].name.should.equal('action 1');
+
+                config.chronos.routes[0].actions[0].should.have.property('type');
+                config.chronos.routes[0].actions[0].type.should.equal('storedProcedure');
+
+                config.chronos.routes[0].actions[1][0].should.have.property('name');
+                config.chronos.routes[0].actions[1][0].name.should.equal('action 2');
+
+                config.chronos.routes[0].actions[1][0].should.have.property('type');
+                config.chronos.routes[0].actions[1][0].type.should.equal('http');
+
+                config.chronos.routes[0].actions[1][1].should.have.property('name');
+                config.chronos.routes[0].actions[1][1].name.should.equal('action 3');
+                config.chronos.routes[0].actions[1][1].should.have.property('type');
+                config.chronos.routes[0].actions[1][1].type.should.equal('http');
+            });
     });
 
-    it('should error if no config file found', function () {
-        var context = {
-            configPath: 'test/mocks/config/i-dont-exist.json',
+    it('should error if no config file found', () => {
+        const context = {
+            configPath: 'test/mocks/config/i-dont-exist.json'
         };
 
         return config(context)
-            .then(function(){
+            .then(() => {
                 throw new Error('method should have thrown.');
             })
-            .catch(function(err) {
-                err.message.should.equal('ENOENT: no such file or directory, open \'test/mocks/config/i-dont-exist.json\'')
-            })
+            .catch((err) => {
+                err.should.be.an('error');
+                err.message.should.equal('ENOENT: no such file or directory, open \'test/mocks/config/i-dont-exist.json\'');
+            });
     });
 
-    it('should error if config is invalid', function () {
-        var context = {
-            configPath: 'test/mocks/config/bad-config.json',
+    it('should error if config is invalid', () => {
+        const context = {
+            configPath: 'test/mocks/config/bad-config.json'
         };
 
         return config(context)
-            .then(function(){
+            .then(() => {
                 throw new Error('method should have thrown.');
             })
-            .catch(function(err) {
-                err.message.should.equal('File contents did not contain expected fields')
-            })
+            .catch((err) => {
+                err.should.be.an('error');
+            });
     });
 });
